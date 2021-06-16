c7c374835bfc77055c82a249d664a7cab9774ac0
thunderkyg <thunderkyg@gmail.com>
2021-06-16 17:57:11 +0900

    chapter05 (2021/06/16)

---
+++ src/com/javaex/ex01/Ex01.java
@@ -0,0 +1,29 @@
+package com.javaex.ex01;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Ex01 {
+
+	public static void main(String[] args) throws IOException {
+
+		InputStream in = new FileInputStream("C:\\javaStudy\\file\\image.jpg");
+		OutputStream out = new FileOutputStream("C:\\javaStudy\\file\\byteImage.jpg");
+
+		int data;
+		System.out.println("복사시작");
+		while (true) {
+			data = in.read();
+			if (data == -1) {
+				System.out.println("복사끝: " + data);
+				break;
+			}
+			out.write(data);
+		}
+
+	}
+
+}
